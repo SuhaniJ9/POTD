d89ad27f2b13bab77555dce1b508bd3f2d57b625
SuhaniJ9 <123815918+SuhaniJ9@users.noreply.github.com>
2024-02-16 18:48:40 +0530

    Create 11feb.cpp

---
+++ 11feb.cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:
+    vector<int> recamanSequence(int n){
+        vector<int>ans(n,0);
+        set<int>s;
+        for(int i=1;i<n;i++){
+            int val=ans[i-1]-i;
+            if(val>0 && s.find(val)==s.end()){
+                s.insert(val);
+                ans[i]=val;
+            }
+            else{
+                s.insert(ans[i-1]+i);
+                ans[i]=ans[i-1]+i;
+            }
+        }
+        
+        return ans; // code here
+    }
+};
