cc8bfa3879d7d416f8283b7c5d1e2c51b4e41de6
SuhaniJ9 <123815918+SuhaniJ9@users.noreply.github.com>
2024-03-30 19:37:59 +0530

    Create 30march.cpp

---
+++ 30march.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int minValue(Node* root) {
+     if(!root) return -1;
+        
+        while(root->left != NULL){
+            root=root->left;
+        }
+        
+        return root->data;
+    }
+};
