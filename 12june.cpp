2c4fedf5006789e78b64d1ac6d189b913280c5b4
SuhaniJ9 <123815918+SuhaniJ9@users.noreply.github.com>
2024-06-12 18:40:20 +0530

    Create 12june.cpp

---
+++ 12june.cpp
@@ -0,0 +1,11 @@
+  int ans=0;
+        for(int i=1;i<=n;i++){
+            string s=to_string(i);
+            for(int x=0; x<s.length();x++){
+                if(s[x]=='4'){
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
